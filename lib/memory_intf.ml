efe1839f42bb8ebba7a1c3e10314daf9f0c2507e
julianayang777 <julianayang2011@gmail.com>
2024-03-14 19:30:58 +0000

    add base case tests for memory

--- lib/memory_intf.ml
+++ lib/memory_intf.ml
@@ -1,20 +1,41 @@
 module type S = sig
   type t
   type value
-  type loc
   type object_
 
   val create : unit -> t
   val clone : t -> t
+
+  (** [insert m o] inserts the object [o] into memory [m] and returns its
+      location. *)
   val insert : t -> object_ -> value
-  val remove : t -> loc -> unit
-  val set : t -> loc -> object_ -> unit
-  val get : t -> loc -> object_ option
-  val set_field : t -> loc -> field:value -> data:value -> unit
-  val get_field : t -> loc -> value -> (value * value list) list
-  val has_field : t -> loc -> value -> value
-  val delete_field : t -> loc -> value -> unit
-  val loc : value -> ((value option * loc) list, string) Result.t
+
+  (** [remove m l] removes the object at location [l] from memory [m]. *)
+  val remove : t -> value -> unit
+
+  (** [set m l o] sets the object at location [l] in memory [m] to objcet [o]. *)
+  val set : t -> value -> object_ -> unit
+
+  (** [get m l o] get the object at location [l] in memory [m]. *)
+  val get : t -> value -> object_ option
+
+  (** [set_field m l ~field ~data] sets the field [field] of the object at
+      location [l] in memory [m] to [data]. *)
+  val set_field : t -> value -> field:value -> data:value -> unit
+
+  (** [get_field m l field] gets the value stored at field [field] of the object
+      at location [l] in memory [m]. *)
+  val get_field : t -> value -> value -> (value * value list) list
+
+  (** [has_field m l field] checks if an object at location [l] in memory [m]
+      has a field [field]. *)
+  val has_field : t -> value -> value -> value
+
+  (** [delete_field m l field] deletes field [field] from object at location [l]
+      in memory [m]. *)
+  val delete_field : t -> value -> value -> unit
+
+  val loc : value -> ((value option * int) list, string) Result.t
   val pp : Fmt.t -> t -> unit
   val pp_val : Fmt.t -> t -> value -> unit
 end
--- lib/memory_symbolic.ml
+++ lib/memory_symbolic.ml
@@ -2,30 +2,39 @@ open Encoding
 
 module Make (O : Object_intf.S with type value = Encoding.Expr.t) = struct
   type value = Encoding.Expr.t
-  type loc = int
   type object_ = O.t
 
   type t =
     { parent : t option
-    ; map : (loc, object_) Hashtbl.t
-    ; mutable next : loc
+    ; map : (int, object_) Hashtbl.t
+    ; mutable next : int
     }
 
   let create () : t = { parent = None; map = Hashtbl.create 512; next = 0 }
   let clone (h : t) : t = { parent = Some h; map = Hashtbl.create 16; next = 0 }
 
+  let get_loc (loc : value) : int =
+    match Expr.view loc with
+    | Val (Int l) -> l
+    | _ -> failwith "memory_symbolic.get_loc: Not a location"
+
   let insert (h : t) (o : object_) : value =
-    Hashtbl.replace h.map h.next o;
+    let next = h.next in
+    Hashtbl.replace h.map next o;
     h.next <- h.next + 1;
-    Expr.(make @@ Val (Int h.next))
+    Expr.(make @@ Val (Int next))
 
-  let remove (h : t) (loc : loc) : unit = Hashtbl.remove h.map loc
+  let remove (h : t) (l : value) : unit =
+    let loc = get_loc l in
+    Hashtbl.remove h.map loc
 
-  let set ({ map = h; _ } : t) (loc : loc) (data : object_) : unit =
+  let set ({ map = h; _ } : t) (l : value) (data : object_) : unit =
+    let loc = get_loc l in
     Hashtbl.replace h loc data
 
-  let find (h : t) (loc : loc) : (object_ * bool) option =
+  let find (h : t) (l : value) : (object_ * bool) option =
     let open Utils.Option in
+    let loc = get_loc l in
     let rec aux { parent; map; _ } loc from_parent =
       match Hashtbl.find_opt map loc with
       | Some o -> Some (o, from_parent)
@@ -35,7 +44,7 @@ module Make (O : Object_intf.S with type value = Encoding.Expr.t) = struct
     in
     aux h loc false
 
-  let get (h : t) (loc : loc) : object_ option =
+  let get (h : t) (loc : value) : object_ option =
     let open Utils.Option in
     let+ obj, from_parent = find h loc in
     match from_parent with
@@ -45,24 +54,24 @@ module Make (O : Object_intf.S with type value = Encoding.Expr.t) = struct
       set h loc obj;
       obj
 
-  let has_field (h : t) (loc : loc) (field : value) : value =
+  let has_field (h : t) (loc : value) (field : value) : value =
     Option.fold (get h loc)
       ~some:(fun o -> O.has_field o field)
       ~none:Expr.(Bool.v false)
 
-  let set_field (h : t) (loc : loc) ~(field : value) ~(data : value) : unit =
+  let set_field (h : t) (loc : value) ~(field : value) ~(data : value) : unit =
     Option.iter
       (fun o ->
         let o' = O.set o ~key:field ~data in
         set h loc o' )
       (get h loc)
 
-  let get_field (h : t) (loc : loc) (field : value) : (value * value list) list
-      =
+  let get_field (h : t) (loc : value) (field : value) :
+    (value * value list) list =
     let o = get h loc in
     Option.fold o ~none:[] ~some:(fun o -> O.get o field)
 
-  let delete_field (h : t) (loc : loc) (f : value) =
+  let delete_field (h : t) (loc : value) (f : value) =
     let obj = get h loc in
     Option.iter
       (fun o ->
@@ -70,7 +79,7 @@ module Make (O : Object_intf.S with type value = Encoding.Expr.t) = struct
         set h loc o' )
       obj
 
-  let rec unfold_ite ~(accum : value) (e : value) : (value option * loc) list =
+  let rec unfold_ite ~(accum : value) (e : value) : (value option * int) list =
     match Expr.view e with
     | Val (Int x) (* | Val (Val.Symbol x)  *) -> [ (Some accum, x) ]
     | Triop (_, Ty.Ite, c, a, e) -> (
@@ -84,7 +93,7 @@ module Make (O : Object_intf.S with type value = Encoding.Expr.t) = struct
       | _ -> assert false )
     | _ -> assert false
 
-  let loc (e : value) : ((value option * loc) list, string) Result.t =
+  let loc (e : value) : ((value option * int) list, string) Result.t =
     match Expr.view e with
     | Val (Int l) -> Ok [ (None, l) ]
     | Triop (_, Ty.Ite, c, a, v) -> (
@@ -95,11 +104,11 @@ module Make (O : Object_intf.S with type value = Encoding.Expr.t) = struct
       )
     | _ -> Error (Fmt.asprintf "Value '%a' is not a loc expression" Expr.pp e)
 
-  let pp_loc (fmt : Fmt.t) (loc : loc) : unit = Fmt.pp_int fmt loc
+  let pp_loc (fmt : Fmt.t) (loc : value) : unit = Expr.pp fmt loc
 
   let rec pp (fmt : Fmt.t) ({ map; parent; _ } : t) =
     let open Fmt in
-    let pp_v fmt (key, data) = fprintf fmt "%a: %a" pp_loc key O.pp data in
+    let pp_v fmt (key, data) = fprintf fmt "%a: %a" pp_int key O.pp data in
     let pp_parent fmt v =
       pp_opt (fun fmt h -> fprintf fmt "%a@ <-@ " pp h) fmt v
     in
@@ -107,14 +116,11 @@ module Make (O : Object_intf.S with type value = Encoding.Expr.t) = struct
       (pp_hashtbl ~pp_sep:pp_comma pp_v)
       map
 
-  let pp_val (fmt : Fmt.t) (h : t) (e : value) : unit =
+  let pp_val (fmt : Fmt.t) (h : t) (l : value) : unit =
     let open Fmt in
-    match Expr.view e with
-    | Val (Int l) -> (
-      match get h l with
-      | None -> fprintf fmt "%a" pp_loc l
-      | Some o -> fprintf fmt "%a -> %a" pp_loc l O.pp o )
-    | _ -> fprintf fmt "%a" Expr.pp e
+    match get h l with
+    | None -> fprintf fmt "%a" pp_loc l
+    | Some o -> fprintf fmt "%a -> %a" pp_loc l O.pp o
 end
 
 module M :
