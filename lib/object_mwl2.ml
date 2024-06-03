81ce8d148bf34294fb5b614b1ff138ef96a63937
julianayang777 <julianayang2011@gmail.com>
2024-06-03 15:39:55 +0100

    object mwl using list type

--- lib/object_mwl2.ml
+++ lib/object_mwl2.ml
@@ -2,7 +2,7 @@ open Utils.Option
 open Utils.Encoding
 open Smtml
 
-(* MWL Model that uses list [r1;r2] *)
+(* MWL Model that record type (cur; Some parent) *)
 module M :
   Object_intf.S with type value = Smtml.Expr.t and type pc_value = Smtml.Expr.t =
 struct
@@ -11,32 +11,31 @@ struct
   type symb_slot = (value * value option) option
   type concrete_table = (string, value option) Hashtbl.t
   type record = { concrete : concrete_table; symbolic : symb_slot }
-    and 
-  t = record list
+  type t = { cur : record; parent : t option }
 
   let create_record () : record =
     { concrete = Hashtbl.create 16; symbolic = None }
 
-  let create () : t = [ create_record () ]
+  let create () : t = { cur = create_record (); parent = None }
 
   let clone (o : t) : t =
     let new_rec = create_record () in
-    new_rec :: o
+    { cur = new_rec; parent = Some o }
 
   let merge (_o1 : t) (_o2 : t) (_pc : pc_value) : t =
     failwith "Object_mwl.merge not implemented"
 
   let set (o : t) ~(field : value) ~(data : value) (pc : pc_value) :
     (t * pc_value) list =
-    let cur = List.hd o in
     match Expr.view field with
     | Val (Str field) ->
-      Hashtbl.replace cur.concrete field (Some data);
+      Hashtbl.replace o.cur.concrete field (Some data);
       [ (o, pc) ]
     | _ ->
-      let new_record = { cur with symbolic = Some (field, Some data) } in
+      let new_record = { o.cur with symbolic = Some (field, Some data) } in
+      let new_o = { o with cur = new_record } in
       let empty_record = create_record () in
-      [ (empty_record :: new_record :: o, pc) ]
+      [ ({ cur = empty_record; parent = Some new_o }, pc) ]
 
   let get_concrete (concrete : concrete_table) (pc : pc_value) (p : value) :
     pc_value option * (pc_value * value option) list =
@@ -72,21 +71,18 @@ struct
     | None -> get_concrete pc p
 
   let rec get_aux (p : value) (pc : pc_value)
-    ((r, pvs) : pc_value option * (pc_value * value option) list) (o : t) :
-    (pc_value * value option) list =
-    match o with
-    | [] -> pvs
-    | cur :: parent -> (
-      let r =
-        match r with
-        | Some r -> r
-        | None -> failwith "object_wlmerge.get_aux unsupported case"
-      in
-      let r', pvs' = get_record cur r p in
-      let pvs'' = pvs @ pvs' in
-      match r' with
-      | None -> pvs''
-      | Some b -> get_aux p pc (Some (and_ r b), pvs'') parent)
+    ((r, pvs) : pc_value option * (pc_value * value option) list)
+    { cur; parent } : (pc_value * value option) list =
+    let r =
+      match r with
+      | Some r -> r
+      | None -> failwith "Object_mwl.get_aux unsupported case"
+    in
+    let r', pvs' = get_record cur r p in
+    let pvs'' = pvs @ pvs' in
+    match r' with
+    | None -> pvs''
+    | Some b -> map_default (get_aux p pc (Some (and_ r b), pvs'')) pvs'' parent
 
   let mk_ite_get (conds : (pc_value * value option) list) : value =
     if List.exists (fun (_, v) -> Option.is_some v) conds then
@@ -101,41 +97,41 @@ struct
     [ (ite_expr, pc) ]
 
   let delete (o : t) (field : value) (pc : pc_value) : (t * pc_value) list =
-    let cur = List.hd o in
     match Expr.view field with
     | Val (Str field) ->
-      Hashtbl.replace cur.concrete field None;
+      Hashtbl.replace o.cur.concrete field None;
       [ (o, pc) ]
     | _ ->
-      let new_record = { cur with symbolic = Some (field, None) } in
+      let new_record = { o.cur with symbolic = Some (field, None) } in
+      let new_o = { o with cur = new_record } in
       let empty_record = create_record () in
-      [ (empty_record :: new_record :: o, pc) ]
+      [ ({ cur = empty_record; parent = Some new_o }, pc) ]
 
   let to_list (o : t) : (value * value) list =
     (* Just calculates if there are just concrete fields *)
-    match o with
-    | [ cur ] ->
+    match o.parent with
+    | Some _ -> assert false
+    | None ->
       Hashtbl.fold
         (fun k v acc -> match v with Some v -> (str k, v) :: acc | None -> acc)
-        cur.concrete []
-    | _ -> assert false
+        o.cur.concrete []
 
   let get_fields (o : t) : value list =
     (* Just calculates if there are just concrete fields *)
-    match o with
-    | [ cur ] ->
+    match o.parent with
+    | Some _ -> assert false
+    | None ->
       Hashtbl.fold
         (fun k v acc -> match v with Some _ -> str k :: acc | None -> acc)
-        cur.concrete []
-    | _ -> assert false
+        o.cur.concrete []
 
   let mk_ite_has_field (conds : (pc_value * value option) list) : value =
     if List.exists (fun (_, v) -> Option.is_some v) conds then
       List.fold_right
         (fun (cond, v) acc ->
-          let v' = map_default (fun _ -> true_) (false_) v in
+          let v' = map_default (fun _ -> true_) false_ v in
           ite cond v' acc )
-        conds (false_)
+        conds false_
     else false_
 
   let has_field (o : t) (field : value) (pc : pc_value) : value =
@@ -143,7 +139,7 @@ struct
     let ite_expr = mk_ite_has_field l in
     ite_expr
 
-  let pp (fmt : Fmt.t) (o : t) : unit =
+  let rec pp (fmt : Fmt.t) (o : t) : unit =
     let open Fmt in
     let pp_v fmt (field, data) =
       fprintf fmt "%a: %a" pp_str field (pp_opt Expr.pp) data
@@ -154,11 +150,11 @@ struct
       | Some (field, data) ->
         fprintf fmt "%a: %a" Expr.pp field (pp_opt Expr.pp) data
     in
-    let pp_record fmt record =
-      fprintf fmt "{{%a}, %a}" pp_concrete record.concrete pp_symbolic
-        record.symbolic
+    let pp_record fmt { concrete; symbolic } =
+      fprintf fmt "{{%a}, %a}" pp_concrete concrete pp_symbolic symbolic
     in
-    fprintf fmt "[ %a ]" (pp_lst ~pp_sep:pp_semicolon pp_record) o
+    let pp_parent fmt v = pp_opt (fun fmt h -> fprintf fmt "%a" pp h) fmt v in
+    fprintf fmt "%a ;@\n%a " pp_record o.cur pp_parent o.parent
 
   let to_string (o : t) : string = Fmt.asprintf "%a" pp o
   let to_json = to_string
